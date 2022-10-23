ec035afc202326e0562b462e80f4979e8dae2bc3
vojtechpavlu <vojtech.pavlu.dev>
2022-10-23 14:04:36 +0200

    Docstring fulfillment

--- src/game/phrase.py
+++ src/game/phrase.py
@@ -1,10 +1,31 @@
-""""""
+"""Tento modul obsahuje prostředky pro práci s tajenkou.
 
+Obsahuje především definici třídy `SecretPhrase`, která má za cíl poskytovat
+službu správy tajenky. Ta se sestává ze znaků, která mají být uhodnuta.
+
+Těmito znaky jsou instance třídy `Letter`, která obsalují jednotlivá písmena
+tajenky a udržují si informaci o tom, zda-li byly již uhodnuty či třeba zda-li
+nejde o speciální znak (např. interpunkční znaménka), který je defaultně
+odkryt."""
+
+# Import knihovny pro práci se znaky s diakritikou
 import unicodedata
 
 
 class Letter:
-    """Instance této třídy reprezentují funkční obal znaku tajenky."""
+    """Instance této třídy reprezentují funkční obal znaku tajenky.
+    Coby obal se v tomto kontextu chová jako bezpečnostní mechanismus, který
+    daný znak ukrývá svým zástupným znakem (viz třídní proměnná `WILDCARD`),
+    aby nebylo vyzrazeno, ale bylo nutné ho uhodnout. Po uhodnutí již tyto
+    instance svůj znak nijak neskrývají.
+
+    Speciálním případem jsou speciální znaky (typicky interpunkční znaménka),
+    která ze své podstaty nemá smysl hádat, proto jsou ve výchozím stavu již
+    od počátku odhalena.
+
+    Díky tomuto mechanismu je možné, aby byla tajenka snadno ukázána a přitom
+    se mohly jednotlivá písmena rozhodnout na základě svého uhodnutí, zda-li
+    se odkryjí či vrátí za sebe zástupný znak."""
 
     # Zástupný znak pro doposud neodhalené písmeno tajenky
     WILDCARD = "_"
@@ -22,7 +43,6 @@ class Letter:
         if len(letter) != 1:
             raise ValueError(f"Povolen je pouze právě jeden znak: '{letter}'!")
 
-        # Skutečně privátní proměnná
         self.__letter = letter.upper()
         self._is_special = is_special
         self._is_revealed = is_special
@@ -78,14 +98,32 @@ class Letter:
 class SecretPhrase:
     """Instance této třídy reprezentují tajenku, která má být uhodnuta.
     Samotná tajenka se sestává z písmen, která jsou obalena instancemi třídy
-    Letter."""
+    Letter.
+
+    Na základě dodané hádané tajenky si tato instance vybuduje posloupnost
+    takto obalených znaků, které se skrývají do té doby, dokud nejsou uhodnuty.
+
+    Tajenka umí na základě dodaného hádaného znaku říci, kolikrát se v tajence
+    vyskytuje (na základě čehož jsou počítány body) a příslušným znakům
+    přenechá odpovědnost za své další skrývání či odkrývání.
+
+    Tajenka se rozhoduje na počátku, které znaky budou od začátku odkryty a
+    které naopak schová - speciální znaky (např. interpunkční znaménka) nemá
+    smysl skrývat, celý seznam takovýchto znaků lze vidět v třídní proměnné
+    `SPECIAL_CHARACTERS`."""
 
     # Speciální znaky, které se nehádají
     SPECIAL_CHARACTERS = [" ", '"', "'", ",", "-", ".", "!", "?"]
 
     def __init__(self, phrase: str):
         """Initor, který přijímá tajnou frázi (tajenku) k uhodnutí.
-        Ta musí být neprázdná, jinak je vyhozena výjimka."""
+        Ta musí být neprázdná, jinak je vyhozena výjimka.
+
+        Kromě testu validity initor odpovídá za vybudování ochranných obalů
+        pro jednotlivá písmena. Ten tvoří rozdělením tajenky na jednotlivé
+        znaky, které poté obaluje instancemi třídy `Letter`. Speciální znaky
+        pak postupuje s příslušnou informací, díky čemuž zůstávají neskrývané
+        už od začátku."""
 
         if len(phrase) == 0:
             raise ValueError(f"Tajenka musí být neprázdná!")
@@ -133,7 +171,13 @@ class SecretPhrase:
 
 
 def remove_accents(string_with_accents: str) -> str:
-    """Funkce odpovědná za odstranění diakritiky."""
+    """Funkce odpovědná za odstranění diakritiky. Díky tomu je možné odhalovat
+    i písmena, která jsou s diakritikou pomocí pokusů se znaky, které takto
+    opatřeny nejsou.
+
+    Například v tajence 'ŘEŘICHA' by pomocí pokusu s písmenem 'R' byly oba
+    výskyty 'Ř' správně odhaleny.
+    """
     return str(
         unicodedata.normalize("NFD", string_with_accents)
         .encode("ascii", "ignore")
