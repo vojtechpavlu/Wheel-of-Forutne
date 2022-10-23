febe5b227be866544136489738dc1475175f5169
vojtechpavlu <vojtech.pavlu.dev>
2022-10-23 15:29:45 +0200

    Docstring fulfillment

--- src/player/abstract_player.py
+++ src/player/abstract_player.py
@@ -1,4 +1,6 @@
-""""""
+"""Tento modul obsahuje definici abstraktní třídy hráče, která je odpovědná
+za sdružování společné funkcionality všech ostatních tříd hráčů a za
+poskytování společné definice protokolu."""
 
 
 from abc import ABC, abstractmethod
@@ -7,23 +9,37 @@ from src.game.game import AbstractGame
 
 
 class Player(ABC):
-    """"""
+    """Abstraktní předek pro všechny hráče, který sdružuje společnou
+    funkcionalitu a poskytuje společný zastřešující protokol pro všechny
+    své potomky."""
 
-    def __init__(self):
-        """"""
+    def __init__(self, player_name: str):
+        """Initor, který přijímá pouze jméno daného hráče."""
+        self._player_name = player_name
+        self._score = 0
 
     @property
-    @abstractmethod
+    def player_name(self) -> str:
+        """Jméno hráče."""
+        return self._player_name
+
+    @property
     def score(self) -> int:
-        """"""
+        """Skóre tohoto hráče."""
+        return self._score
 
     @score.setter
-    @abstractmethod
-    def score(self, new_score) -> int:
-        """"""
+    def score(self, new_score: int):
+        """Nastavuje skóre hráče."""
+        self._score = new_score
 
     @abstractmethod
     def guess_letter(self, game: AbstractGame) -> str:
-        """"""
+        """Abstraktní metoda, která definuje požadovanou signaturu pro každého
+        hráče. Implementace této metody je odpovědná za pokus o uhodnutí
+        dalšího písmene v tajence.
+
+        K tomu dostává referenci na instanci hry, ze které se může dozvědět
+        například již použité znaky, nebo počet chybějících písmen."""
 
 
