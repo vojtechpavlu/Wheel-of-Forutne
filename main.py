3d4e34a245a18da29b145db310d544b03550fbd9
vojtechpavlu <vojtech.pavlu.dev>
2022-10-23 20:28:46 +0200

    Simple testing environment

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
-from src.game import default_wheel
+from src.game import default_wheel, wheel_without_bankrupts
 from src.game.moderator import Moderator
-from src.game.game import SinglePlayerGame
+from src.game.game import SinglePlayerGame, MultiplayerGame
+from src.player.entropy_driven_player import (
+    EntropyDrivenPlayerCZ, EntropyDrivenPlayerEN)
 from src.player.human_player import HumanPlayer
 
-phrase = "TO BE OR NOT TO BE"
-
+phrase = "Poslušně hlásím, že jsem zase tady."
 wheel = default_wheel()
-player = HumanPlayer("Karel")
-game = SinglePlayerGame(phrase, wheel, player)
-moderator = Moderator(game)
+players = [HumanPlayer("Já"), EntropyDrivenPlayerCZ()]
+game = MultiplayerGame(phrase, wheel, players)
+moderator = Moderator(game, True)
 
 moderator.run_game()
