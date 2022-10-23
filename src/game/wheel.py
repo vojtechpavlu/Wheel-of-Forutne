d54fcf8273793f17fa0bf14d389817875f50b882
vojtechpavlu <vojtech.pavlu.dev>
2022-10-23 14:15:40 +0200

    Moved access functions for default values in __init__.py

--- src/game/wheel.py
+++ src/game/wheel.py
@@ -72,24 +72,3 @@ class Wheel:
         """Simulace točení kola štěstí. Metoda náhodně vybere jeden klín,
         který vrací."""
         return choice(self.wedges)
-
-
-def create_wedge(multiplier: int) -> Wedge:
-    """Pomocná funkce, která vygeneruje výherní klín pro dodaný multiplikátor.
-    """
-    return Wedge(f"${multiplier}", multiplier)
-
-
-def create_bankrupt_wedge() -> Wedge:
-    """Pomocná funkce, která vygeneruje klín bankrotu."""
-    return Wedge(Wedge.BANKRUPT_NAME, 0)
-
-
-_DEFAULT_WEDGES = [
-    create_wedge(800), create_wedge(500), create_wedge(650), create_wedge(500),
-    create_wedge(500), create_wedge(900), create_wedge(700), create_wedge(600),
-    create_wedge(800), create_wedge(500), create_wedge(700), create_wedge(500),
-    create_wedge(600), create_wedge(550), create_wedge(500), create_wedge(900),
-    create_wedge(650), create_wedge(900), create_wedge(900), create_wedge(300),
-    create_wedge(700), create_bankrupt_wedge(), create_bankrupt_wedge(),
-]
